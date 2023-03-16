6178de8491adcd79fc1106c546bd00dc85eae2a7
tuladhar2002 <tuladhar002@gannon.edu>
2023-03-16 16:12:31 -0400

    second commit

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iiostream>
+
+using namespace std;
+
+int main(){
+	cout << "Hello World" << endl
+	return 0;
+}
+
---
+++ script2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Hello Again!"
+echo "Date is $(date)"
+echo "Current directory is $(pwd)"
+
